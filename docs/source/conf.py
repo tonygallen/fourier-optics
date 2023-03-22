116838cb11bb65f4efba7073850d9626678745a5
Tony Allen <25624821+tonygallen@users.noreply.github.com>
2023-03-22 17:13:48 -0400

    remove un-used _static folder

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -119,8 +119,3 @@ napoleon_use_rtype = False
 # html_theme = "faculty-sphinx-theme"
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
